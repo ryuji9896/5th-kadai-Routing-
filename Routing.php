dd4774a92ee269924cb1f3f6eea1db9e8b9410de
ryuji9896 <makinoryujijoker9896@i.softbank.jp>
2023-12-19 14:05:13 +0000

    フォルダの作成とRoutingの変更

--- Routing.php
+++ Routing.php
@@ -4,7 +4,7 @@ http://XXXXXX.jp/XXXにアクセスが来たら
 Controller AAAのAction bbbに渡す
 (prefixは問題文通りであれば不要ですがより有用な形にするために使用しました。)
 */
-use App\Http\Controllers\AAA;
+use App\Http\Controllers\AAAController;
 Route::Controller(AAA::class)->prefix('XXX')->group(function() {
     Route::get('','bbb');
 });
