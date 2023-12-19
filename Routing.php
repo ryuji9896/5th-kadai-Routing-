eae29fa918cf3b6869d1b175944002e3f19e983a
ryuji9896 <makinoryujijoker9896@i.softbank.jp>
2023-12-19 13:50:07 +0000

    first commit

---
+++ Routing.php
@@ -0,0 +1,10 @@
+<?php
+/*4:回答
+http://XXXXXX.jp/XXXにアクセスが来たら
+Controller AAAのAction bbbに渡す
+(prefixは問題文通りであれば不要ですがより有用な形にするために使用しました。)
+*/
+use App\Http\Controllers\AAA;
+Route::Controller(AAA::class)->prefix('XXX')->group(function() {
+    Route::get('','bbb');
+});
